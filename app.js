300ba12d7b135b27a4c93ac54d547817c4cf3a76
Batzaya0112 <Batzaya8971@gmail.com>
2021-02-26 19:55:27 +0800

    uicontrolleriig holbono

--- app.js
+++ app.js
@@ -2,7 +2,24 @@
 //Дэлгэцтэй ажиллах 
 var uiController = (function()
 {
-
+      var DOMstrings = {
+        inputType: ".add__type", 
+        inputDescription: ".add__description",
+        inputValue: ".add__value",
+        addBtn: ".add__btn"
+      };
+      return {
+        getInput: function(){
+         return{
+           type: document.querySelector(DOMstrings.inputType).value,
+           description: document.querySelector(DOMstrings.inputDescription).value,
+           value: document.querySelector(DOMstrings.inputValue).value
+         }
+        },
+        getDOMstrings: function(){
+          return DOMstrings;
+        }
+      }
 })(); 
 
 
@@ -14,9 +31,10 @@ var financeController = (function(){
 //eventListner bolon zavsriin code
 var appController = (function(uiController, financeController)
 {
+  var DOM = uiController.getDOMstrings();
     var controllerAddItem = function(){
-        // 1. oruulah ugugdliih delgetsees olj avna.
-
+      // 1. oruulah ugugdliih delgetsees olj avna.
+      console.log(uiController.getInput());
       // 2. Olj avsan ugugdluuddee sankhuugiin controllert damjuulj tend khadgalna.
 
       // 3. Olj avsan ugugdluudee veb deerkh tokhirokh khesegt n gargana.
@@ -24,9 +42,9 @@ var appController = (function(uiController, financeController)
       // 4. Tusviig tootsoolno/
     
       // 5. Etssiin uldegdel, tootsoog delgetseng gargana.
-      console.log('klsdhkha');
+      
     }
-  document.querySelector('.add__btn').addEventListener('click', function(){
+  document.querySelector(DOM.addBtn).addEventListener('click', function(){
       controllerAddItem();
   });
 
