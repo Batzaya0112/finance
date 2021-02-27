d85bf52aadee1c2dc2132463228c9e0e74f45431
Batzaya0112 <Batzaya8971@gmail.com>
2021-02-27 20:42:08 +0800

    setUpListeners fn baiguulna

--- app.js
+++ app.js
@@ -31,8 +31,8 @@ var financeController = (function(){
 //eventListner bolon zavsriin code
 var appController = (function(uiController, financeController)
 {
-  var DOM = uiController.getDOMstrings();
-    var controllerAddItem = function(){
+  
+  var controllerAddItem = function(){
       // 1. oruulah ugugdliih delgetsees olj avna.
       console.log(uiController.getInput());
       // 2. Olj avsan ugugdluuddee sankhuugiin controllert damjuulj tend khadgalna.
@@ -43,15 +43,26 @@ var appController = (function(uiController, financeController)
     
       // 5. Etssiin uldegdel, tootsoog delgetseng gargana.
       
-    }
-  document.querySelector(DOM.addBtn).addEventListener('click', function(){
-      controllerAddItem();
-  });
+  }
 
-  document.addEventListener('keypress', function(event){
-    if(event.keyCode == 13 || event.which == 13){
+   var  setupEventListeners = function(){
+      var DOM = uiController.getDOMstrings();
+      document.querySelector(DOM.addBtn).addEventListener('click', function(){
         controllerAddItem();
-    }
-  });
+      });
+
+      document.addEventListener('keypress', function(event){
+        if(event.keyCode == 13 || event.which == 13){
+          controllerAddItem();
+      }
+      });
+   }
+   return {
+     init: function(){
+      setupEventListeners();
+     }
+   }
 
 })(uiController, financeController); 
+
+appController.init();
