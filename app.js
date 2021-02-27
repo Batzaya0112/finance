a4b87376666de4da7e2d9b903c990545ad7bfa51
Batzaya0112 <Batzaya8971@gmail.com>
2021-02-27 20:52:45 +0800

    sankhuutei ajillaj fn-g programchilna

--- app.js
+++ app.js
@@ -25,7 +25,28 @@ var uiController = (function()
 
 //Санхүүтай ажиллах
 var financeController = (function(){
-  
+
+    var data = {
+      allItems: {
+        inc: [],
+        exp: []
+      },
+      totals:{
+        inc: 0,
+        exp: 0
+      }
+    }
+   
+    var Income = function(id, description, value){
+      this.id = id;
+      this.description = description;
+      this.value = value;
+    }
+    var Expense = function(id, description, value){
+      this.id = id;
+      this.description = description;
+      this.value = value;
+    }
 })();
 
 //eventListner bolon zavsriin code
